df306cb12f9518983d182ee8b27b46fe070e4504
Andrey Pestunov <masterlifting@gmail.com>
2023-03-27 13:22:28 +0200

    feat: add async debounce service

--- Gizmo.UI/Services/DebounceAsyncServiceBase.cs
+++ Gizmo.UI/Services/DebounceAsyncServiceBase.cs
@@ -8,54 +8,57 @@ namespace Gizmo.UI.Services;
 /// <remarks>Disposable.</remarks>
 public abstract class DebounceAsyncServiceBase<T> : IDisposable
 {
-
     #region CONSTRUCTOR
     protected DebounceAsyncServiceBase(ILogger logger) => Logger = logger;
     #endregion
 
     #region PRIVATE FIELDS
+    Timer? _timer;
+    private int _debounceBufferTime = 1000; // 1 sec by default
     private record DebounceItem(T Item, CancellationToken CancelationToken);
     private readonly ConcurrentDictionary<object, DebounceItem> _items = new();
-    Timer? _timer;
-    private readonly Random _random = new();
     #endregion
 
     #region PROPERTIES
     protected ILogger Logger { get; }
-    public int DebounceBufferTime { get; set; } = 5000; // 1 sec by default
+    public int DebounceBufferTime
+    {
+        get { return _debounceBufferTime; }
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentOutOfRangeException(nameof(DebounceBufferTime));
+
+            _debounceBufferTime = value;
+
+            //reschedule timer
+            _timer ??= new Timer(GetTimerCallback(), null, _debounceBufferTime, Timeout.Infinite);
+        }
+    }
     #endregion
 
     #region PUBLIC FUNCTIONS
     public void Debounce(T item, CancellationToken cToken = default)
     {
-        _timer ??= new Timer(GetTimerCallback(), null, DebounceBufferTime, Timeout.Infinite);
+        _timer ??= new Timer(GetTimerCallback(), null, _debounceBufferTime, Timeout.Infinite);
 
         var key = GetKey(item);
         var value = new DebounceItem(item, cToken);
 
         _items.AddOrUpdate(key, value, (_, __) => value);
-
-        Console.WriteLine($"Debounce item {key} was added or updated to the dictionary.");
     }
 
     public void Dispose() => _timer?.Dispose();
     #endregion
 
     #region PRIVATE FUNCTIONS
-    private TimerCallback GetTimerCallback() => async _ =>
-    {
-        await Task.Run(async () =>
+    private TimerCallback GetTimerCallback() => async _ => await Task.Run(async () =>
         {
-            Console.WriteLine("Debounce processing timer callback is starting.");
-
             await ProcessItems();
 
             _timer?.Dispose();
             _timer = null;
-
-            Console.WriteLine("Debounce processing timer callback was completed.");
         });
-    };
     private async Task ProcessItems()
     {
         List<Task> tasks = new(_items.Count);
@@ -64,9 +67,6 @@ public abstract class DebounceAsyncServiceBase<T> : IDisposable
         {
             if (_items.TryRemove(element.Key, out _))
             {
-                if (_random.Next(0, 100) > 50)
-                    element.Value.CancelationToken.ThrowIfCancellationRequested();
-
                 if (element.Value.CancelationToken.IsCancellationRequested)
                 {
                     Logger.LogWarning("Debounce processing item {0} was cancelled.", element.Key);
@@ -74,33 +74,19 @@ public abstract class DebounceAsyncServiceBase<T> : IDisposable
                 }
 
                 tasks.Add(Task.Run(() => OnDebounce(element.Value.Item, element.Value.CancelationToken)));
-
-                Console.WriteLine($"Debounce processing item {element.Key} was added to the tasks.");
-            }
-            else
-            {
-                Console.WriteLine($"Debounce process of the item {element.Key} was not removed.");
             }
         }
-
-        Console.WriteLine("Debounce processing tasks of the items were starting.");
 
         await Task.WhenAll(tasks).ContinueWith(task =>
         {
             if (task.IsFaulted)
-            {
                 Logger.LogError(task.Exception, "Debounce processing tasks of the items were failed.");
-            }
-            else
-            {
-                Console.WriteLine("Debounce processing tasks of the items were completed.");
-            }
         });
     }
     #endregion
 
     #region ABSTRACT FUNCTIONS
-    public abstract Task OnDebounce(T item, CancellationToken cToken = default);
-    public abstract object GetKey(T item);
+    protected abstract Task OnDebounce(T item, CancellationToken cToken);
+    protected abstract object GetKey(T item);
     #endregion
 }
--- Gizmo.UI/View/Services/ViewStateDebounceService.cs
+++ Gizmo.UI/View/Services/ViewStateDebounceService.cs
@@ -6,24 +6,21 @@ using Microsoft.Extensions.Logging;
 namespace Gizmo.UI.View.Services;
 
 /// <inheritdoc/>
-public sealed class ViewStateDebounceService : DebounceAsyncServiceBase<IViewState>
+public sealed class ViewStateDebounceService : DebounceServiceBase<IViewState>
 {
-    private readonly Random _random = new();
     public ViewStateDebounceService(ILogger<ViewStateDebounceService> logger) : base(logger)
     {
     }
 
-    public override object GetKey(IViewState item) => item.GetHashCode();
+    protected override void OnDebounce(IViewState item) => item.RaiseChanged();
 
-    public override async Task OnDebounce(IViewState item, CancellationToken cToken = default)
-    {
-        await Task.Delay(_random.Next(1000, 5000), cToken);
-        var isException = _random.Next(0, 100) > 50;
+    //for asyn debounce
 
-        if (isException)
-            throw new NotImplementedException($"This is a test exception for the {item.GetHashCode()}.");
+    // protected override object GetKey(IViewState item) => item.GetHashCode();
 
-        item.RaiseChanged();
-        Console.WriteLine($"Debounce item {item.GetHashCode()} method was called.");
-    }
+    // protected override Task OnDebounce(IViewState item, CancellationToken cToken = default)
+    // {
+    //     item.RaiseChanged();
+    //     return Task.CompletedTask;
+    // }
 }
