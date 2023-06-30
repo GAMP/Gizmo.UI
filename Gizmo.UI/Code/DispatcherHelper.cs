96361a5cda8e65e10ac7c58692bccaf5fc6589bf
Oleg Savelos <olegsavelos@gmail.com>
2023-06-30 13:21:00 +0300

    invoke on dispatcher.

--- Gizmo.UI/Code/DispatcherHelper.cs
+++ Gizmo.UI/Code/DispatcherHelper.cs
@@ -11,15 +11,16 @@ namespace Gizmo.UI
     /// </remarks>
     public static class DispatcherHelper
     {
-        private static readonly bool _isWebBrowser = RuntimeInformation.IsOSPlatform(OSPlatform.Create("browser"));
+        private static readonly bool IsWebBrowser = RuntimeInformation.IsOSPlatform(OSPlatform.Create("browser"));
 
-        private static readonly object? _dispatcher = default; //dispatcher class
-        private static readonly MethodInfo? _invokeAsyncMethod = default;
-        private static readonly PropertyInfo? _taskProperty = default;
+        private static readonly object? Dispatcher = default; //dispatcher class
+        private static readonly MethodInfo? InvokeAsyncMethod = default;
+        private static readonly MethodInfo? InvokeMethod = default;
+        private static readonly PropertyInfo? TaskProperty = default;
 
         static DispatcherHelper()
         {
-            if(!_isWebBrowser)
+            if(!IsWebBrowser)
             {
                 var applicationType = Type.GetType("System.Windows.Application,PresentationFramework",true);
                 var dispatcherType = Type.GetType("System.Windows.Threading.Dispatcher,WindowsBase",true);
@@ -28,9 +29,13 @@ namespace Gizmo.UI
                 var currentProperty = applicationType?.GetProperty("Current", BindingFlags.Static | BindingFlags.Public);
                 var application = currentProperty?.GetValue(null);
                 var dispatcherProperty = applicationType?.GetProperty("Dispatcher", BindingFlags.Public | BindingFlags.Instance);
-                _dispatcher = dispatcherProperty?.GetValue(application);
-                _invokeAsyncMethod = dispatcherType?.GetMethod("InvokeAsync", new[] { typeof(Action) });
-                _taskProperty = dispatcherOperationType?.GetProperty("Task", BindingFlags.Public | BindingFlags.Instance);
+                Dispatcher = dispatcherProperty?.GetValue(application);
+                InvokeAsyncMethod = dispatcherType?.GetMethod("InvokeAsync", new[] { typeof(Action) });
+                InvokeMethod = dispatcherType?.GetMethods()
+                    .Where(m=> m.Name =="Invoke" && m.ReturnType.Name =="TResult")
+                    .First()
+                    .MakeGenericMethod(typeof(object));
+                TaskProperty = dispatcherOperationType?.GetProperty("Task", BindingFlags.Public | BindingFlags.Instance);
             }
         }
 
@@ -42,12 +47,26 @@ namespace Gizmo.UI
         public static async Task InvokeAsync(Action action)
         {
             //this operation is only supported on wpf host
-            if (_isWebBrowser)
+            if (IsWebBrowser)
                 throw new NotSupportedException();
 
-            var result = _invokeAsyncMethod?.Invoke(_dispatcher, new[] { action });
-            if(result!= null && _taskProperty?.GetValue(result) is Task task)
+            var result = InvokeAsyncMethod?.Invoke(Dispatcher, new[] { action });
+            if(result!= null && TaskProperty?.GetValue(result) is Task task)
                 await task;           
+        }
+
+        /// <summary>
+        /// Invokes specified action on current dispatcher.
+        /// </summary>
+        /// <param name="action">Action.</param>
+        /// <exception cref="NotSupportedException">is thrown in case this method invoked in non desktop environment.</exception>
+        public static T? Invoke<T>(Func<T> action)
+        {
+            //this operation is only supported on wpf host
+            if (IsWebBrowser)
+                throw new NotSupportedException();
+
+            return (T?)InvokeMethod?.Invoke(Dispatcher, new[] { action });
         }
     }
 }
