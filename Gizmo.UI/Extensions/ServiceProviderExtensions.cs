d3af7c69fe83e7c9cd73c10e2c85597a0f285e71
Oleg Savelos <olegsavelos@gmail.com>
2023-07-18 10:22:55 +0300

    added logging.

--- Gizmo.UI/Extensions/ServiceProviderExtensions.cs
+++ Gizmo.UI/Extensions/ServiceProviderExtensions.cs
@@ -1,8 +1,16 @@
 ﻿using Gizmo.UI.View.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Gizmo.UI
 {
+    /// <summary>
+    /// Helper class for log category mapping.
+    /// </summary>
+    public sealed class UIServiceProviderExtensions
+    {
+    }
+
     /// <summary>
     /// Service provider extensions.
     /// </summary>
@@ -18,13 +26,18 @@ namespace Gizmo.UI
         /// <returns>Associated task.</returns>
         public static async Task InitializeViewsServices(this IServiceProvider serviceProvider, CancellationToken ct = default)
         {
+            //create logger
+            var logger = serviceProvider.GetRequiredService<ILogger<UIServiceProviderExtensions>>();
+
             //get view services
             var viewServices = serviceProvider.GetServices<IViewService>();
 
             //initialize view services
             foreach (var service in viewServices)
             {
+                logger.LogTrace("Initializing view service {s}.", service);
                 await service.InitializeAsync(ct);
+                logger.LogTrace("Initialization of view service {s} completed.", service);
             }
         } 
 
