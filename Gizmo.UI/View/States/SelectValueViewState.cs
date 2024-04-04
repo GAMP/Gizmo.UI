cd02236403aaef57e944a34b8638532dfb6506fa
Oleg Savelos <olegsavelos@gmail.com>
2024-04-04 18:27:30 +0300

    updates.

---
+++ Gizmo.UI/View/States/SelectValueViewState.cs
@@ -0,0 +1,15 @@
+﻿namespace Gizmo.UI.View.States
+{
+    public sealed class SelectValueViewState<TValueType> : ViewStateBase
+    {
+        /// <summary>
+        /// Display value.
+        /// </summary>
+        public string DisplayValue { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Selected value.
+        /// </summary>
+        public TValueType Value { get; set; } = default!;
+    }
+}
