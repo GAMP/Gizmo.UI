e9b73e31bf35f736a227070fc8365398f9949885
Oleg Savelos <olegsavelos@gmail.com>
2025-12-05 14:02:03 +0200

    notifications updates.

--- Gizmo.UI/Services/Models/AddComponentResultCode.cs
+++ Gizmo.UI/Services/Models/AddComponentResultCode.cs
@@ -9,25 +9,30 @@
     public enum AddComponentResultCode
     {
         /// <summary>
-        /// Component was succesfuly added.
+        /// Component was successfully added.
         /// </summary>
         Opened,
+
         /// <summary>
         /// Component was closed with result.
         /// </summary>
         Ok,
+
         /// <summary>
-        /// Component was canceld by one of cancellation tokens without result.
+        /// Component was canceled by one of cancellation tokens without result.
         /// </summary>
         Canceled,
+
         /// <summary>
         /// Component was not added.
         /// </summary>
         Failed,
+
         /// <summary>
         /// Component timed out.
         /// </summary>
         TimeOut,
+
         /// <summary>
         /// Component was dismissed without result.
         /// </summary>
