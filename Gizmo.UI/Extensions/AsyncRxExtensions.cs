64352ce3e1a5b0d7be258ada9f10d62f613b5738
Oleg Savelos <olegsavelos@gmail.com>
2026-01-05 08:54:03 +0200

    move extensions.

---
+++ Gizmo.UI/Extensions/AsyncRxExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace System.Reactive.Linq
+{
+    public static class AsyncRxExtensions
+    {
+        public static IObservable<T> AsyncWhereSequential<T>(this IObservable<T> source, Func<T, CancellationToken, Task<bool>> predicateAsync)
+        {
+            return source
+                .Select(x =>
+                    Observable.FromAsync(ct => predicateAsync(x, ct))
+                              .Where(pass => pass)     // keep only passed
+                              .Select(_ => x))         // project back the original item
+                .Concat();                             // process sequentially
+        }
+
+        public static IObservable<T> AsyncWhereConcurrent<T>(this IObservable<T> source, Func<T, CancellationToken, Task<bool>> predicateAsync, int maxConcurrency = 4)
+        {
+            return source
+                .Select(x =>
+                    Observable.FromAsync(ct => predicateAsync(x, ct))
+                              .Where(pass => pass)
+                              .Select(_ => x))
+                .Merge(maxConcurrency); // parallel up to N
+        }
+    }
+}
